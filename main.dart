0031526e31203fd409ddae81abac9d00b21177cb
abdallah hejazi <test@youremail.com>
2022-08-27 16:40:13 +0300

    the update

--- main.dart
+++ main.dart
@@ -1,5 +1,24 @@
-void main() {
-  String myName = "abdallah";
 
-  print(myName);
+void main() {
+
+  Map myInfo ={
+
+  "first_name " : "Abdallah",
+  "last_name" :"Alyattawi",
+  "age": 22,
+ };
+
+  Map myPaintings = {
+    "DaVinci":"MonaLisa" , 
+    "vanGogh":"TheStarryNight" , 
+    "Sandro Botticelli":["theSpring","VenusAndMars"]};
+
+     
+
+  print(myInfo);
+  print(myPaintings);
+  print("after add new collection");
+ myPaintings.addAll({"portraitOfDante":"theMystical"});
+
+  
 }
